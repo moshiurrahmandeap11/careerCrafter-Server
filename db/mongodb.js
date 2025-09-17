5488eac4d36f845a2d22a86ce5a16d0316bbf120
sharifulislamudoy <surifroton301@gmail.com>
2025-09-17 22:49:07 +0600

    connect mongodb with the server

---
+++ db/mongodb.js
@@ -0,0 +1,25 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { mongoURI } = require('../config/config');
+
+
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectDB() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB!");
+    return client;
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
